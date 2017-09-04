7a25c144280e4e91546f0365e0b9304977b9f4b2
murkymao <glaciefer.1@gmail.com>
2017-09-04 13:17:18 +0300

    start project

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Leo Landing</title>
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <header class="clearfix">
+        <img src="assets/img/Arrow%20left%20in%20a%20circle.png" alt="left-arrow" class="left-arrow">
+        <section class="column column-2">
+            <span class="header-title">Leo/-</span>
+        </section>
+        <section class="header-navigation-block column column-2">
+            <nav>
+                <ul>
+                    <li><a href="#">Solutions</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </section>
+        <div class="clearfix"></div>
+        <section class="header-title column column-2">
+            <img src="assets/img/icon.png" alt="logo" class="header-logo">
+        </section>
+        <section class="header-info">
+            <h1>Introducing</h1>
+            <h2>Something hot......</h2>
+            <button type="submit" class="button">Try Demo</button>
+        </section>
+        <a href="#"><img src="assets/img/Arrow%20right%20in%20a%20circle.png" alt="right-arrow" class="right-arrow"></a>
+    </header>
+    <div class="clearfix"></div>
+    <div class="content">
+        <div class="column column-3 border"><a href="#"><img src="assets/img/phone.png" alt=""></a></div>
+        <div class="column column-3 border"><a href="#"><img src="assets/img/basket.png" alt=""></a></div>
+        <div class="column column-3 border"><a href="#"><img src="assets/img/earth.png" alt=""></a></div>
+        <div class="column column-3 border"><a href="#"><img src="assets/img/search.png" alt=""></a></div>
+        <div class="clearfix"></div>
+        <div class="ipad"><img src="assets/img/ipad.png" alt="ipad">
+            <h3>Lorem Ipsum is simply dummy text</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap
+                into electronic typesetting, remaining essentially unchanged. <br> <br>
+
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and
+                more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+                Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                been
+                the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                type
+                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                with
+                the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="border"></div>
+        <div class="column column-2 bottom"><img src="assets/img/screen_shot.png" alt=""></div>
+        <div class="column column-2 bottom">
+            <h4>Why choose us ?</h4>
+            <ul>
+                <li><a href="#">Lorem Ipsum is simply dummy text of the printing and typesetting</a></li>
+                <li><a href="#">Lorem Ipsum has been the industry's standard dummy text ever</a></li>
+                <li><a href="#">When an unknown printer took a galley of type and scrambled</a></li>
+                <li><a href="#">It has survived not only five centuries, but also the leap into electronic </a></li>
+                <li><a href="#">It was popularised in the 1960s with the release of Letraset sheets containing</a>
+                </li>
+                <li><a href="#">More recently with desktop publishing software like Aldus PageMaker</a></li>
+            </ul>
+        </div>
+        <div class="clearfix"></div>
+        <hr>
+        <div class="column column-2 login">
+            <p>Member login</p>
+            <input type="text" name="username" placeholder="Username"><input type="password" name="password"
+                                                                             placeholder="Password">
+            <a href="#"><button>Log me in</button></a>
+        </div>
+        <div class="column column-2 email">
+            <p>Subscribe to our weekly newsletter</p>
+            <input type="email" name="email" placeholder="Email adress">
+            <a href="#"><button>Subscribe</button></a>
+        </div>
+    </div>
+</div>
+<footer>
+    <div class="column column-3">
+        <h5>About Us</h5>
+        <ul>
+            <li><a href="#">Abouts us</a></li>
+            <li><a href="#">Why us</a></li>
+            <li><a href="#">Customer Stories</a></li>
+            <li><a href="#">Press Resources</a></li>
+            <li><a href="#">Press Releases</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="column column-3">
+        <h5>About Us</h5>
+        <ul>
+            <li><a href="#">Abouts us</a></li>
+            <li><a href="#">Why us</a></li>
+            <li><a href="#">Customer Stories</a></li>
+            <li><a href="#">Press Resources</a></li>
+            <li><a href="#">Press Releases</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </div>
+    <div class="column column-3">
+        <h5>Testimonials</h5>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industr
+            standard dummy text ever since the 1500s,
+            when an unknown printer took a
+        </p>
+        <span>
+                Lorem Ipsum is simply <br>
+                Owner, Lorem Ipsum
+        </span>
+    </div>
+    <div class="column column-3"><span class="header-title">Leo/-</span></div>
+    <div class="clearfix"></div>
+    <div class="copy"><span>&copy;murkymao</span></div>
+</footer>
+
+
+</body>
+</html>
